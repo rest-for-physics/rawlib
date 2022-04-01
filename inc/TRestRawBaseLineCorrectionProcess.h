611ad516c08dc73715ef77f72d756e49a21cb510
Konrad Altenmueller <konrad@altenmueller.net>
2022-04-01 15:38:11 +0200

    New process TRestRawBaseLineCorrectionProcess

---
+++ inc/TRestRawBaseLineCorrectionProcess.h
@@ -0,0 +1,74 @@
+/*************************************************************************
+ * This file is part of the REST software framework.                     *
+ *                                                                       *
+ * Copyright (C) 2016 GIFNA/TREX (University of Zaragoza)                *
+ * For more information see http://gifna.unizar.es/trex                  *
+ *                                                                       *
+ * REST is free software: you can redistribute it and/or modify          *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * REST is distributed in the hope that it will be useful,               *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have a copy of the GNU General Public License along with   *
+ * REST in $REST_PATH/LICENSE.                                           *
+ * If not, see http://www.gnu.org/licenses/.                             *
+ * For the list of contributors see $REST_PATH/CREDITS.                  *
+ *************************************************************************/
+
+#ifndef RESTProc_TRestRawBaseLineCorrectionProcess
+#define RESTProc_TRestRawBaseLineCorrectionProcess
+
+#include "TRestRawSignalEvent.h"
+#include "TRestRawSignalEvent.h"
+#include "TRestEventProcess.h"
+
+class TRestRawBaseLineCorrectionProcess : public TRestEventProcess {
+private:
+    // We define specific input/output event data holders
+    TRestRawSignalEvent* fInputEvent;  //!
+    TRestRawSignalEvent* fOutputEvent;  //!
+
+    void Initialize();
+
+    /// It defines the signals id range where analysis is applied
+    TVector2 fSignalsRange = TVector2(-1, -1);
+    
+    /// Time window width in bins for the moving average filter for baseline correction
+	Int_t fSmoothingWindow = 75;
+
+    /// Just a flag to quickly determine if we have to apply the range filter
+    Bool_t fRangeEnabled = false;  //!
+
+public:
+    any GetInputEvent() { return fInputEvent; }
+    any GetOutputEvent() { return fOutputEvent; }
+
+    void InitProcess();
+
+    TRestEvent* ProcessEvent(TRestEvent* eventInput);
+
+    void EndProcess();
+
+    void PrintMetadata() {
+        BeginPrintProcess();
+
+        metadata << "Smoothing window size: " << fSmoothingWindow << endl;
+        metadata << "Baseline correction applied to signals with IDs in range (" << fSignalsRange.X() << "," << fSignalsRange.Y() << ")" << endl; 
+
+        EndPrintProcess();
+    }
+
+    TRestRawBaseLineCorrectionProcess();
+    ~TRestRawBaseLineCorrectionProcess();
+
+    // ROOT class definition helper. Increase the number in it every time
+    // you add/rename/remove the process parameters
+    ClassDef(TRestRawBaseLineCorrectionProcess, 1);
+
+};
+#endif
