4a5b3b03601616f1b0c9cb992f253bc57f9bf00e
juanan <juanan318@gmail.com>
2023-05-18 12:44:54 +0200

    Moving structs inside TRestTRestRawBiPoToSignalProcess class and using constexpr

--- inc/TRestRawBiPoToSignalProcess.h
+++ inc/TRestRawBiPoToSignalProcess.h
@@ -26,59 +26,61 @@
 #include "TRestRawSignalEvent.h"
 #include "TRestRawToSignalProcess.h"
 
-static const size_t CTAG_SZ = 4;
+constexpr size_t CTAG_SZ = 4;
 
-const int MATACQ_N_CH = 4;               // Number of channels
-const int MATACQ_MAX_DATA_SAMP = 10240;  // Max number of samples for
-const int MATACQ_BIPO_TIMEOUT = 0;       // BiPo mode timeout
+constexpr int MATACQ_N_CH = 4;               // Number of channels
+constexpr int MATACQ_MAX_DATA_SAMP = 10240;  // Max number of samples for
+constexpr int MATACQ_BIPO_TIMEOUT = 0;       // BiPo mode timeout
 
-static const int MATACQ_UNDERFLOW = 0x0000;
-static const int MATACQ_ZERO = 0x8000;
-static const int MATACQ_OVERFLOW = 0xFFFF;
+constexpr uint32_t MATACQ_UNDERFLOW = 0x0000;
+constexpr uint32_t MATACQ_ZERO = 0x8000;
+constexpr uint32_t MATACQ_OVERFLOW = 0xFFFF;
 
-const std::string TAG_RUN_START = "STA";
-const std::string TAG_RUN_BIPO = "ST2";
-const std::string TAG_RUN_STOP = "STO";
-const std::string TAG_ACQ = "ACQ";
-const std::string TAG_ACQ_2 = "AC2";
+constexpr char TAG_RUN_START[] = "STA";
+constexpr char TAG_RUN_BIPO[] = "ST2";
+constexpr char TAG_RUN_STOP[] = "STO";
+constexpr char TAG_ACQ[] = "ACQ";
+constexpr char TAG_ACQ_2[] = "AC2";
 
-/// A structure to store the configuration settings of Matacq board
-struct MatacqBoard {
-    /// The base memory address of the Matacq board
-    int32_t address;
-
-    int32_t en_ch[MATACQ_N_CH];
-    int32_t trg_ch[MATACQ_N_CH];
-
-    int32_t Trig_Type;
-    int32_t Threshold;
-    int32_t Nb_Acq;
-    int32_t Posttrig;
-    int32_t Time_Tag_On;
-    int32_t Sampling_GHz;
-
-    int32_t ch_shifts[MATACQ_N_CH];
-    int32_t nChannels;
-};
-
-/// A structure to store the BiPo settings
-struct BiPoSettings {
-    int32_t trigger_address;
-
-    int32_t Win1_Posttrig;
-    int32_t Timeout_200KHz;
-
-    int32_t Trig_Chan[MATACQ_N_CH];
-    int32_t Level1_mV[MATACQ_N_CH];
-    int32_t Level2_mV[MATACQ_N_CH];
-
-    int32_t t1_window;
-    int32_t t2_window;
-    int32_t t1_t2_timeout;
-};
 
 //! An process to read binary data from BiPo electronics
 class TRestRawBiPoToSignalProcess : public TRestRawToSignalProcess {
+   public:
+   /// A structure to store the configuration settings of Matacq board
+   struct MatacqBoard {
+     /// The base memory address of the Matacq board
+     int32_t address;
+
+     std::array<int32_t, MATACQ_N_CH> en_ch;
+     std::array<int32_t, MATACQ_N_CH> trg_ch;
+
+     int32_t Trig_Type;
+     int32_t Threshold;
+     int32_t Nb_Acq;
+     int32_t Posttrig;
+     int32_t Time_Tag_On;
+     int32_t Sampling_GHz;
+
+     std::array<int32_t, MATACQ_N_CH> ch_shifts;
+     int32_t nChannels;
+   };
+
+   /// A structure to store the BiPo settings
+   struct BiPoSettings {
+     int32_t trigger_address;
+
+     int32_t Win1_Posttrig;
+     int32_t Timeout_200KHz;
+
+     std::array<int32_t, MATACQ_N_CH> Trig_Chan;
+     std::array<int32_t, MATACQ_N_CH> Level1_mV;
+     std::array<int32_t, MATACQ_N_CH> Level2_mV;
+
+     int32_t t1_window;
+     int32_t t2_window;
+     int32_t t1_t2_timeout;
+   };
+
    protected:
     /// The number of Matacq boards present on the setup
     Int_t fNBoards = 0;  ///<
@@ -96,7 +98,7 @@ class TRestRawBiPoToSignalProcess : public TRestRawToSignalProcess {
     void ReadFooter();
     void ReadBoard();
     void ReadBiPoSetup();
-    Int_t ReadBiPoEventData(uint16_t* mdata);
+    Int_t ReadBiPoEventData(std::vector<uint16_t> &mdata);
 
     UInt_t GetBoardIndex(Int_t address);
     Int_t GetBin(Int_t boardIndex, Int_t channel, Int_t bin);
